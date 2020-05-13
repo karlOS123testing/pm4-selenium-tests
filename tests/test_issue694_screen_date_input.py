53b7548f29523c39072c1163b4491faffc493987
Kelly Hodis <kelly.hodis@processmaker.com>
2020-05-13 15:03:18 -0700

    Add test skeleton for issue 694

---
+++ tests/test_issue694_screen_date_input.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+
+'''
+These lines will run locally, but will not currently work in the Docker image.
+They define the path so that imports from parallel folders can be found.
+
+from sys import path
+path.append('../')
+'''
+
+import unittest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from test_parent import BaseTest
+from util import run_test, login
+
+'''
+These are in the format necessary when editing the local path.
+
+from includes.test_parent import BaseTest
+from includes.util import run_test, login
+'''
+
+
+class TestScreenDateInput(BaseTest):
+    ''' Test date input on Screens behaves as expected. '''
+
+    def test_screen_date_input(self):
+        ''' Test that user can login and the Designer page displays. '''
+
+        # Login using configured url, workspace, username, and password
+        self.driver = login(data['server_url'], data['username'], data['password'], self.driver)
+
+        # Wait for Processes page to load
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'sidebar-inner')))
+
+        # Verify that inner sidebar is displayed
+        self.assertTrue(self.driver.find_element_by_id('sidebar-inner').is_displayed())
+
+
+if __name__ == "__main__":
+    import __main__
+    output = run_test(TestLogin, data, __main__)
