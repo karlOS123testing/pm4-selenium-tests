180ef4e96a009972121c33595825e8a8fa5805e9
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-03 11:33:31 -0700

    Add test for edting user's job title

---
+++ tests/test-admin/test-users/test_TCP4_609.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3
+""" Class to edit a user's job title.
+"""
+
+# Check if using local environment
+from os import getenv
+
+if getenv('ENVIRONMENT') != 'local':
+    from test_parent import BaseTest
+    from util import run_test, login
+# If using local environment
+else:
+    from sys import path
+    path.append('../')
+    from includes.test_parent import BaseTest
+    from includes.util import run_test, login
+    from __init__ import data
+
+import unittest
+import string
+import random
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestUserJobTitleEdited(BaseTest):
+    ''' Navigate to the Users page and edit a user's job title. '''
+
+    def test_that_user_job_title_is_edited(self):
+        ''' Verify that a user's job title is successfully edited.'''
+        
+        # Login using configured url, workspace, username, and password
+        self.driver = login(data['server_url'], data['username'], data['password'], self.driver)
+
+        # Redirect to Admin Users page, wait for +User button to be clickable 
+        self.driver.get(data['server_url'] + '/admin/users')
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'vuetable-body')))
+        
+        # Find table record with ID = '2', find edit button in this element, and click
+        user_tr = self.driver.find_element_by_xpath("//tr//td[1][contains(text(), '2')]").find_element_by_xpath("..")
+        user_tr.find_element_by_class_name('fa-pen-square').click()
+        
+        # Wait for user edit form to load, change job title, and save
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'title')))
+        self.driver.find_element_by_id('title').clear()
+        job_title = ''.join(random.choice(string.ascii_letters) for n in range(10))
+        self.driver.find_element_by_id('title').send_keys(job_title)
+        self.driver.find_element_by_id('saveUser').click()
+
+        # Navigate to Users page and wait for Users table to load and reclick table record edit button
+        self.driver.get(data['server_url'] + '/admin/users')
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'vuetable-body')))
+        user_tr = self.driver.find_element_by_xpath("//tr//td[1][contains(text(), '2')]").find_element_by_xpath("..")
+        user_tr.find_element_by_class_name('fa-pen-square').click()
+
+        # Wait for edit user form to load and verify job title field contains correct job title
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'title')))
+        job_title_field = self.driver.find_element_by_id('title')
+        self.assertEqual(job_title_field.get_property('value'), job_title)
+
+
+if __name__ == "__main__":
+    import __main__
+    output = run_test(TestUserJobTitleEdited, data, __main__)
