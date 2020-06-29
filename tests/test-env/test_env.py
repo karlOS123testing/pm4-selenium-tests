c05f8c3ac64def81d3a8481cafe2edd7c843bbaa
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-29 11:47:50 -0700

    Add test for docker ENV variable

---
+++ tests/test-env/test_env.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+""" Class to test the new auth client name field.
+"""
+
+# Check if using local environment
+from os import getenv
+
+if getenv('ENVIRONMENT') == 'docker':
+    from test_parent import BaseTest
+    from util import run_test, login
+# If using local environment
+else:
+    from sys import path
+    path.append('../')
+    from includes.test_parent import BaseTest
+    from includes.util import run_test, login
+    from __init__ import data
+
+import unittest
+import string
+import random
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestEnv(BaseTest):
+    ''' Navigate to the Requests page. '''
+
+    def test_that_environment_check_works(self):
+        ''' Verify that ENV variable passed to Docker container works as a test for environment.'''
+        
+        # Login using configured url, workspace, username, and password
+        self.driver = login(data['server_url'], data['username'], data['password'], self.driver)
+
+        # Wait for Requests page to load until inner sidebar is displayed and assert it is
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'sidebar-inner')))
+        self.assertTrue(self.driver.find_element_by_id('sidebar-inner').is_displayed())
+
+
+if __name__ == "__main__":
+    import __main__
+    output = run_test(TestEnv, data, __main__)
+    
