6fce3bcb1797ab423e3b01f5b60c1a0aa2fe205c
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-22 15:32:56 -0700

    Remove imports

--- includes/page_menu.py
+++ includes/page_menu.py
@@ -5,27 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-# Check if using local environment
-from os import getenv
-
-#if getenv('ENVIRONMENT') != 'local':
- #   from test_parent import BaseTest
-  #  from util import run_test
-   # from page_login import PageLogin
-    #from page_users import PageUsers
-    #from page_menu import PageMenu
-
-    
-# If using local environment
-#else:
-from sys import path
-path.append('../')
-from includes.test_parent import BaseTest
-from includes.util import run_test
-from includes.page_login import PageLogin
-from __init__ import data
-from includes.page_users import PageUsers
-
 
 class PageMenu:
     ''' Page object model for Navigation Menu. '''
