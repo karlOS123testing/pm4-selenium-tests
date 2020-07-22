1804ca88d3bc5561d068c4527f1d20669c54d000
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-22 16:16:40 -0700

    Add comments

--- includes/page_users.py
+++ includes/page_users.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-# functions for the POM POC
+""" Users Page class. """
 
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
@@ -9,19 +9,23 @@ class PageUsers:
     ''' Page object model for users page'''
 
     def __init__(self, driver, data):
+        ''' Instantiate PageUsers object. '''
         self.driver=driver
         self.data=data
         self.wait = WebDriverWait(driver, 30)
     
     def paths_users(self):
+        ''' Function to get page elements. '''
         self.user_search_bar = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Search']"))) 
         self.non_admin_edit = self.wait.until(EC.visibility_of_element_located((By.XPATH, "(//button[@title='Edit'])[2]"))) 
 
     def search_long_string(self):
+        ''' Function to input a long string in the search user bar. '''
         self.paths_users()
         self.user_search_bar.send_keys('qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjklñzxcvbnmqwerty')
         return (self.user_search_bar.get_property('value') == 'qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjklñzxcvbnmqwerty') 
 
     def edit_non_admin(self):
+        ''' Function to edit a non admin user. '''
         self.paths_users()
         self.non_admin_edit.click()
