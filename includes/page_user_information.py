8aad784198e0aa513946da378cdde2d874552c0e
zltm <jdavidrevillad@gmail.com>
2020-07-22 16:17:53 -0400

    page_user_information updated to add languages, user_permissions created for english language

--- includes/page_user_information.py
+++ includes/page_user_information.py
@@ -17,6 +17,25 @@ class PageUserInformation:
     def paths_user_information(self):
         self.user_permissions_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-profile']")))   
 
+        self.user_language = self.wait.until(EC.visibility_of_element_located((By.ID, "language")))   
+        #self.user_language_deutch = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='de']")))   
+        self.user_language_english = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='en']")))   
+        #self.user_language_spanish = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='es']")))   
+        #self.user_language_french = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='fr']")))   
+
     def goto_user_permissions(self):
         self.paths_user_information()
         self.user_permissions_tab.click()
+
+    def change_user_language(self, selected_language):
+        self.paths_user_information()
+        self.user_language.click()
+        self.switch_language(selected_language)
+
+    def switch_language(self, selected_language):
+        switcher = {
+            #"deutch": self.user_language_deutch.click(),
+            "english": self.user_language_english.click(),
+            #"spanish": self.user_language_spanish.click(),
+            #"french": self.user_language_french.click(),
+        }
---
+++ includes/page_user_permissions.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+# functions for the POM POC
+
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class PageUserPermissions:
+    ''' Page object model for user information page'''
+
+    def __init__(self, driver, data):
+        self.driver = driver
+        self.data = data
+        self.wait = WebDriverWait(driver, 30)
+
+    def paths_user_permissions(self):
+        self.user_information_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-home']")))       
+
+        self.english_auth_create_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Create Auth Clients')]")))
+        self.english_auth_delete_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Delete Auth Clients')]")))
+        self.english_auth_edit_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Edit Auth-Clients')]")))
+        self.english_auth_see_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'View Auth-Clients')]")))
+
+        self.english_permissions_items = self.english_auth_create_client + self.english_auth_delete_client + self.english_auth_edit_client + self.english_auth_see_client
+
+    def goto_user_permissions(self):
+        self.paths_user_permissions()
+        self.user_information_tab.click()
+
+
+
+    def check_permissions_english_translation(self):
+        self.paths_user_permissions()
+
+        if(self.english_permissions_items == 4):
+            return True
+
+        else:
+            return False
