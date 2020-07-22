52828f91b1ce91e384b77a9740b506aac3487f3a
zltm <jdavidrevillad@gmail.com>
2020-07-22 16:26:08 -0400

    user_permissions now include try check

--- includes/page_user_permissions.py
+++ includes/page_user_permissions.py
@@ -4,6 +4,7 @@
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class PageUserPermissions:
@@ -17,18 +18,22 @@ class PageUserPermissions:
     def paths_user_permissions(self):
         self.user_information_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-home']")))       
 
-        self.english_auth_create_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Create Auth Clients')]")))
-        self.english_auth_delete_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Delete Auth Clients')]")))
-        self.english_auth_edit_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Edit Auth-Clients')]")))
-        self.english_auth_see_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'View Auth-Clients')]")))
+        try:
+            self.english_auth_create_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Create Auth Clients')]")))
+            self.english_auth_delete_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Delete Auth Clients')]")))
+            self.english_auth_edit_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Edit Auth-Clients')]")))
+            self.english_auth_see_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'View Auth-Clients')]")))
+        except TimeoutException:
+            self.english_auth_create_client = 0
+            self.english_auth_delete_client = 0
+            self.english_auth_edit_client = 0
+            self.english_auth_see_client = 0
 
         self.english_permissions_items = self.english_auth_create_client + self.english_auth_delete_client + self.english_auth_edit_client + self.english_auth_see_client
 
     def goto_user_permissions(self):
         self.paths_user_permissions()
         self.user_information_tab.click()
-
-
 
     def check_permissions_english_translation(self):
         self.paths_user_permissions()
