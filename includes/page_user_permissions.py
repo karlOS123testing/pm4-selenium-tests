776c9d72a6de5ce369460ec907bfcf2f0214b39c
zltm <jdavidrevillad@gmail.com>
2020-07-22 16:47:47 -0400

    user_permissions page working

--- includes/page_user_permissions.py
+++ includes/page_user_permissions.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import TimeoutException
 
 
 class PageUserPermissions:
-    ''' Page object model for user information page'''
+    ''' Page object model for user permissions page'''
 
     def __init__(self, driver, data):
         self.driver = driver
@@ -16,29 +16,32 @@ class PageUserPermissions:
         self.wait = WebDriverWait(driver, 30)
 
     def paths_user_permissions(self):
-        self.user_information_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-home']")))       
+        self.user_information_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-home']")))              
+        self.auth_accordeon = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@data-target= '#auth-clients']")))     
 
+        #Looks for the translated strings on permissions
         try:
             self.english_auth_create_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Create Auth Clients')]")))
             self.english_auth_delete_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Delete Auth Clients')]")))
             self.english_auth_edit_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'Edit Auth-Clients')]")))
             self.english_auth_see_client = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[contains(text(), 'View Auth-Clients')]")))
-        except TimeoutException:
-            self.english_auth_create_client = 0
-            self.english_auth_delete_client = 0
-            self.english_auth_edit_client = 0
-            self.english_auth_see_client = 0
 
-        self.english_permissions_items = self.english_auth_create_client + self.english_auth_delete_client + self.english_auth_edit_client + self.english_auth_see_client
+            self.bool_english_permissions_items = True
+        except TimeoutException:
+            self.bool_english_permissions_items = False
 
     def goto_user_permissions(self):
         self.paths_user_permissions()
         self.user_information_tab.click()
 
+    def open_auth_accordeon(self):
+        self.paths_user_permissions()
+        self.auth_accordeon.click()
+
     def check_permissions_english_translation(self):
         self.paths_user_permissions()
 
-        if(self.english_permissions_items == 4):
+        if self.bool_english_permissions_items:
             return True
 
         else:
