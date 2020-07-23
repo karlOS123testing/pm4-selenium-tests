6983d6c20fbf9537ec8ad6ce02674dd45a69d4dc
zltm <jdavidrevillad@gmail.com>
2020-07-23 11:19:42 -0400

    PEP8 implemented

--- includes/page_user_information.py
+++ includes/page_user_information.py
@@ -17,13 +17,13 @@ class PageUserInformation:
 
     def paths_user_information(self):
         ''' Function to get page elements. '''
-        self.user_permissions_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-profile']")))   
+        self.user_permissions_tab = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#nav-profile']")))
 
-        self.user_language = self.wait.until(EC.visibility_of_element_located((By.ID, "language")))   
-        #self.user_language_deutch = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='de']")))   
-        self.user_language_english = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='en']")))   
-        #self.user_language_spanish = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='es']")))   
-        #self.user_language_french = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='fr']")))   
+        self.user_language = self.wait.until(EC.visibility_of_element_located((By.ID, "language")))
+        # self.user_language_deutch = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='de']")))
+        self.user_language_english = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='en']")))
+        # self.user_language_spanish = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='es']")))
+        # self.user_language_french = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//option[@value='fr']")))
 
     def goto_user_permissions(self):
         self.paths_user_information()
@@ -36,8 +36,8 @@ class PageUserInformation:
 
     def switch_language(self, selected_language):
         switcher = {
-            #"deutch": self.user_language_deutch.click(),
+            # "deutch": self.user_language_deutch.click(),
             "english": self.user_language_english.click(),
-            #"spanish": self.user_language_spanish.click(),
-            #"french": self.user_language_french.click(),
+            # "spanish": self.user_language_spanish.click(),
+            # "french": self.user_language_french.click()
         }
