5d49cc6de165cd0c674fe4d95a7720aabe9352c4
zltm <jdavidrevillad@gmail.com>
2020-07-28 15:58:34 -0400

    assert added to 283

--- tests/test_FOUR_283.py
+++ tests/test_FOUR_283.py
@@ -4,27 +4,30 @@
 # Check if using local environment
 from os import getenv
 
-if getenv('ENVIRONMENT') != 'local':
+""" if getenv('ENVIRONMENT') != 'local':
     from test_parent import BaseTest
     from util import run_test
     from page_login import PageLogin
     from page_users import PageUsers
+    from page_user_information import PageUserInformation
     from page_menu import PageMenu
-    from includes.page_users import PageUsers
-    from includes.page_user_information import PageUserInformation
-    from includes.page_menu import PageMenu
-    from includes.prerequisites import Prerequisites
-    from includes.page_user_permissions import PageUserPermissions
+    from prerequisites import Prerequisites
+    from page_user_permissions import PageUserPermissions """
 
     
 # If using local environment
-else:
-    from sys import path
-    path.append('../')
-    from includes.test_parent import BaseTest
-    from includes.util import run_test
-    from includes.page_login import PageLogin
-    from __init__ import data
+""" else: """
+from sys import path
+path.append('../')
+from includes.test_parent import BaseTest
+from includes.util import run_test
+from includes.page_login import PageLogin
+from includes.page_users import PageUsers
+from includes.page_user_information import PageUserInformation
+from includes.page_menu import PageMenu
+from includes.prerequisites import Prerequisites
+from includes.page_user_permissions import PageUserPermissions
+from __init__ import data
 
 
 class Four283(BaseTest):
@@ -47,11 +50,12 @@ class Four283(BaseTest):
         PageUserInformation(self.driver, data).change_user_language("english")
         PageUserInformation(self.driver, data).goto_user_permissions()
 
-        # Insert the 60 characters string on the searchbar
-        PageUserPermissions(self.driver, data).check_permissions_english_translation
+        # Insert the 60 characters string on the searchbar        
+        self.assertTrue(PageUserPermissions(self.driver, data).check_permissions_english_translation())
 
         
         
 if __name__ == "__main__":
     import __main__
     output = run_test(Four283, data, __main__)
+    print(output)
