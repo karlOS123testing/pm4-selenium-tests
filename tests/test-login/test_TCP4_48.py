4f28c86e7eb4dc6d38eca37a918d1708bc4c8972
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-04 12:19:41 -0700

    Implement Page Object

--- tests/test-login/test_TCP4_48.py
+++ tests/test-login/test_TCP4_48.py
@@ -3,46 +3,45 @@
 """
 
 # Check if using local environment
-from os import getenv
-
-if getenv('ENVIRONMENT') != 'local':
-    from test_parent import BaseTest
-    from util import run_test, login
-# If using local environment
-else:
-    from sys import path
-    path.append('../')
-    from includes.test_parent import BaseTest
-    from includes.util import run_test, login
+from os import getenv, path
+if getenv('ENVIRONMENT') == 'local':
     from __init__ import data
+    from sys import path
+    path.append('../includes')
 
-import unittest
-import string
 import random
+import string
+from test_parent import BaseTest
+import util
+from page import *
+import unittest
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class TestLoginPageUsernameField(BaseTest):
     ''' Navigate to the login page and enter text into the username field. '''
 
-    def test_that_login_username_field_accepts_long_strings(self):
-        ''' Verify that a string 61+ characters long will be accepted in the username field.'''
-        # Navigate to server
+    def setUp(self):
+        # Load server url
         self.driver.get(data['server_url'])
 
-        # Wait for login page to load
-        self.wait.until(EC.element_to_be_clickable((By.NAME, 'username')))
+    def test_that_login_username_field_accepts_long_strings(self):
+        ''' Verify that a string 61+ characters long will be accepted in the username field.'''
+        # Load login page.
+        login_page = LoginPage(self.driver, data)
 
-        # Create long user name and enter into username field
-        long_user_name = ''.join(random.choice(string.ascii_letters) for n in range(65))
-        self.driver.find_element_by_id('username').send_keys(long_user_name)
+        # Check if browser is at login page
+        assert login_page.is_url_matches()
 
-        # Verify username field contains the long user name
-        username_field = self.driver.find_element_by_id('username')
-        self.assertEqual(long_user_name, username_field.get_property('value'))
+        # Enter long string in username field
+        long_username = util.generate_long_text()
+        login_page.username_field_element = long_username
+
+        self.assertEqual(login_page.username_field_element, long_username)
 
 
 if __name__ == "__main__":
     import __main__
-    output = run_test(TestLoginPageUsernameField, data, __main__)
+    output = util.run_test(TestLoginPageUsernameField, data, __main__)
