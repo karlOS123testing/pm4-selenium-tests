c90bf07d4a04d52b022b11f44142ac3cb2cd922a
zltm <jdavidrevillad@gmail.com>
2020-08-20 14:21:31 -0400

    branch for TCP4_1 now using the master architecture

---
+++ includes/page_menu.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+""" Page Navigation class. """
+
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class PageMenu:
+    ''' Page object model for Navigation Menu. '''
+
+    def __init__(self, driver, data):
+        ''' Instantiate PageMenu object. '''
+        self.driver = driver
+        self.data = data
+        self.wait = WebDriverWait(self.driver, 30)
+
+    def goto_admin(self):
+        ''' Function to navigate to the admin tab. '''
+        self.driver.get(self.data['server_url'] + '/admin/users')
+
+    def goto_request(self):
+        ''' Function to navigate to the request tab. '''
+        self.driver.get(self.data['server_url'] + '/admin/request')
