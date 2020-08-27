ad497eb3a7bcbeae2ba309b1be9fa23058afdb27
Carlos Camacho <carlos.camacho@processmaker.com>
2020-08-27 19:24:59 -0400

    Change the url

--- tests/test_TCP4-870.py
+++ tests/test_TCP4-870.py
@@ -17,16 +17,15 @@ class TestLoginUser(BaseTest):
     def setUp(self):
         # Load server url
         self.driver.get(data['server_url'])
+ 
 
     def test_correct_login(self):
         '''TCS4_870 Verify the login for an active user'''
         # Load login page.
         login_page = LoginPage(self.driver, data)
         login_page.login()
-        
-        urlUser = self.driver.current_url
-
-        self.assertEqual(urlUser,'https://release-testing.processmaker.net/requests')
+        # Verify the URL        
+        self.assertEqual(self.driver.current_url,data['server_url'] + 'requests')
 
 if __name__ == "__main__":
     import __main__
