cba3f0c129666a54bc3513fbe5a45a83f83e7a6a
Carlos Camacho <carlos.camacho@processmaker.com>
2020-08-27 14:43:50 -0400

    test

---
+++ tests/test_TCP4-870.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+# Check if using local environment
+
+from os import getenv, path
+from sys import path
+path.append('../includes')
+from __init__ import data
+from test_parent import BaseTest
+import util
+from page import *
+import unittest
+
+###########
+class TestLoginUser(BaseTest):
+
+    def setUp(self):
+        # Load server url
+        self.driver.get(data['server_url'])
+
+    def test_correct_login(self):
+        '''TCS4_870 Verify the login for an active user'''
+        # Load login page.
+        login_page = LoginPage(self.driver, data)
+        login_page.login()
+        
+        urlUser = self.driver.current_url
+
+        self.assertEqual(urlUser,'https://release-testing.processmaker.net/requests')
+
+if __name__ == "__main__":
+    import __main__
+    output = util.run_test(TestLoginUser, data, __main__)
+    print(output)
+
+#########
