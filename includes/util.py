02fcf2138dd6c6d02416a02e08ec090382e4f594
zltm <jdavidrevillad@gmail.com>
2020-08-28 11:12:37 -0400

    updated imports

--- includes/util.py
+++ includes/util.py
@@ -1,13 +1,20 @@
 #!/usr/local/bin/python3
+from os import getenv
+# Check if using local environment
+if getenv('ENVIRONMENT') == 'local':
+    # Import sys.path to add the /includes directory to the path
+    # This matches the docker executor's path so local test imports match
+    # remote Trogdor test imports
+    from sys import path
+    path.append('../includes')
+    # Import __init__ to include data configuration
+    from __init__ import data
+
+
 """ Module to contain helper functions that cut down on redundant code in main calls.
 """
-from os import getenv
 
-if getenv('ENVIRONMENT') != 'local':
-    from test_classes import CustomTextTestRunner, CustomTestLoader
-# If using local environment
-else:
-    from includes.test_classes import CustomTextTestRunner, CustomTestLoader
+from includes.test_classes import CustomTextTestRunner, CustomTestLoader
 
 
 import random
@@ -15,7 +22,6 @@ import string
 from contextlib import redirect_stdout
 from io import StringIO
 import re
-from test_classes import CustomTextTestRunner, CustomTestLoader
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """
